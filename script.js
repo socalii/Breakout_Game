8d9a8e6c05dd0dda746d3292ba1f57bcf1808656
Susan Choi <2539lee@gmail.com>
2022-04-10 18:35:44 -0700

    Move Paddle

--- script.js
+++ script.js
@@ -74,8 +74,6 @@ function drawScore() {
   ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
 }
 
-console.log(bricks)
-
 // Draw bricks on canvas
 function drawBricks() {
   bricks.forEach((column) => {
@@ -89,15 +87,68 @@ function drawBricks() {
   })
 }
 
+// Move paddle on canvas
+function movePaddle() {
+  paddle.x += paddle.dx
+
+  // Wall detection
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w
+  }
+
+  if (paddle.x < 0) {
+    paddle.x = 0
+  }
+}
+
 // Draw everything
 function draw() {
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall()
   drawPaddle()
   drawScore()
   drawBricks()
 }
 
-draw()
+// Update canvas drawing and animation
+function update() {
+  movePaddle()
+
+  // Draw everything
+  draw()
+
+  requestAnimationFrame(update)
+}
+
+update()
+
+// Keydown event
+function keyDown(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    paddle.dx = paddle.speed
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    paddle.dx = -paddle.speed
+  }
+}
+
+// Keyup event
+function keyUp(e) {
+  if (
+    e.key === 'Right' ||
+    e.key === 'ArrowRight' ||
+    e.key === 'Left' ||
+    e.key === 'ArrowLeft'
+  ) {
+    paddle.dx = 0
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    paddle.dx = -paddle.speed
+  }
+}
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown)
+document.addEventListener('keyup', keyUp)
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'))
